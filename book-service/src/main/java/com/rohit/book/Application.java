23f58593b098c2f114b4857d61ae44a9a5172b68
Rohit Raj Thirumurthy <rohitrajt@gmail.com>
2017-02-24 18:45:48 -0800

    Adding Book Service basic spring boot code

---
+++ book-service/src/main/java/com/rohit/book/Application.java
@@ -0,0 +1,21 @@
+package com.rohit.book;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by rohitrajt on 2/24/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        logger.info("Book Service is up and running");
+    }
+}
